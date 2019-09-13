bfc0f4810b4a479dd33371cf46f79d9c75029d97
Josh Smith <joshsmith01.contact@gmail.com>
2019-09-13 14:21:04 -0700

    Convert color strings to HSL in state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import colorString from 'color-string';
+import Color from 'color';
 
-function App() {
+const App = () => {
+  const [colors, setColors] = useState({
+    colorStart: '#eeeeee',
+    colorEnd: '#ff0000'
+  });
+  const [colorsHsl, setColorsHsl] = useState({
+    colorStart: {},
+    colorEnd: {}
+  });
+
+  const handleChange = (event) => {
+    event.persist();
+    setColors(previousValues => ({
+      ...previousValues,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let colorStartHsl = Color(colors.colorStart);
+    let colorEndHsl = Color(colors.colorEnd);
+    setColorsHsl(colorsHsl => ({
+      ...colorsHsl,
+      colorStart: colorStartHsl,
+      colorEnd: colorEndHsl
+    }));
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="colorStart" placeholder="Base Color #dad" onChange={handleChange} />
+        <input type="text"  name="colorEnd" placeholder="Result Color #ee00ff" onChange={handleChange} />
+        <input type="submit" value="Sassify"  />
+      </form>
+      <div className="color-display">
+        <p>Click the color to copy its value</p>
+        <div className="color-card color-card-start" style={{background: colors.colorStart}}>
+          Start Color {colors.colorStart}
+        </div>
+        <div className="color-card color-card-end" style={{background: colors.colorEnd}}>
+          End Color {colors.colorEnd}
+        </div>
+      </div>
     </div>
   );
 }
