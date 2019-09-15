7b26487a93e651f007606d2f497c85bbe5b128df
Josh Smith <joshsmith01.contact@gmail.com>
2019-09-14 17:46:59 -0700

    Return the color function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
-import colorString from 'color-string';
 import Color from 'color';
 
 const App = () => {
@@ -12,6 +11,7 @@ const App = () => {
     colorStart: [],
     colorEnd: []
   });
+  const [colorFunctionArray, setColorFunctionArray] = useState([]);
 
   const handleChange = (event) => {
     event.persist();
@@ -33,37 +33,66 @@ const App = () => {
   };
 
   useEffect(() => {
-     getColorDifferences(colorsHsl.colorStart, colorsHsl.colorEnd)
+    getColorDifferences(colorsHsl.colorStart, colorsHsl.colorEnd);
+    getColorFunction(colors.colorStart, differences);
   }, [colorsHsl]);
 
-
+  var differences = [];
   function getColorDifferences(start, end) {
-    let differences = [];
     for (let i = 0; i < 3; i++) {
       differences[i] = start[i] - end[i];
     }
-    console.log(differences);
-    return ( differences );
+    console.log('differences: ',differences);
+    return (differences);
   }
 
+  function getColorFunction(colorString, hslDifferences, mode) {
+    // default mode: sass
+    var mode = typeof mode !== 'undefined' ? mode : "sass";
+    var colorFunction = colorString;
+    // H
+    if (hslDifferences[0] !== 0) { // if hue changes
+      var invH = hslDifferences[0] * -1;
+      var hueFunction = (mode === "sass") ? "adjust-hue" : "spin";
+      colorFunction = hueFunction + "( " + colorFunction + ", " + invH + "deg )";
+    }
+    // S
+    if (hslDifferences[1] < 0) { // if second color is more saturated
+      var absS = Math.abs(hslDifferences[1]);
+      colorFunction = "saturate( " + colorFunction + ", " + absS + " )";
+    } else if (hslDifferences[1] > 0) { // if second color is less saturated
+      colorFunction = "desaturate( " + colorFunction + ", " + hslDifferences[1] + " )";
+    }
+    // L
+    if (hslDifferences[2] < 0) { // if second color is lighter
+      var absL = Math.abs(hslDifferences[2]);
+      colorFunction = "lighten( " + colorFunction + ", " + absL + " )";
+    } else if (hslDifferences[2] > 0) { // if second color is darker
+      colorFunction = "darken( " + colorFunction + ", " + hslDifferences[2] + " )";
+    }
+    // Logs the hslDifferences to the console in an array. -JMS
+    console.log('colorFunction: ',colorFunction);
+    setColorFunctionArray([...colorFunctionArray, colorFunction]);
+    return (colorFunction);
+  }
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="colorStart" placeholder="Base Color #dad" onChange={handleChange} />
-        <input type="text"  name="colorEnd" placeholder="Result Color #ee00ff" onChange={handleChange} />
-        <input type="submit" value="Sassify"  />
-      </form>
-      <div className="color-display">
-        <p>Click the color to copy its value</p>
-        <div className="color-card color-card-start" style={{background: colors.colorStart}}>
-          Start Color {colors.colorStart}
-        </div>
-        <div className="color-card color-card-end" style={{background: colors.colorEnd}}>
-          End Color {colors.colorEnd}
+      <div className="App">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="colorStart" placeholder="Base Color #dad" onChange={handleChange}/>
+          <input type="text" name="colorEnd" placeholder="Result Color #ee00ff" onChange={handleChange}/>
+          <input type="submit" value="Sassify"/>
+        </form>
+        <div className="color-display">
+          <p>Click the color to copy its value</p>
+          <div className="color-card color-card-start" style={{background: colors.colorStart}}>
+            Start Color {colors.colorStart}
+          </div>
+          <div className="color-card color-card-end" style={{background: colors.colorEnd}}>
+            End Color {colors.colorEnd}
+          </div>
         </div>
       </div>
-    </div>
   );
 }
 
